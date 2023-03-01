17a030145be5b7fedf71e64b01fdf40d390860cc
云层上的光 <15115359055@139.com>
2023-03-01 21:59:55 +0800

    feat: uni-app 第一次项目跑起来~

--- main.js
+++ main.js
@@ -2,49 +2,43 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
+
 Vue.config.productionTip = false
-App.mpType = 'app'
+App.mpType = 'app';
+import store from './store';
 
 try {
-  function isPromise(obj) {
-    return (
-      !!obj &&
-      (typeof obj === "object" || typeof obj === "function") &&
-      typeof obj.then === "function"
-    );
-  }
+    function isPromise(obj) {
+        return (
+            !!obj &&
+            (typeof obj === "object" || typeof obj === "function") &&
+            typeof obj.then === "function"
+        );
+    }
 
-  // 统一 vue2 API Promise 化返回格式与 vue3 保持一致
-  uni.addInterceptor({
-    returnValue(res) {
-      if (!isPromise(res)) {
-        return res;
-      }
-      return new Promise((resolve, reject) => {
-        res.then((res) => {
-          if (res[0]) {
-            reject(res[0]);
-          } else {
-            resolve(res[1]);
-          }
-        });
-      });
-    },
-  });
-} catch (error) { }
+    // 统一 vue2 API Promise 化返回格式与 vue3 保持一致
+    uni.addInterceptor({
+        returnValue(res) {
+            if (!isPromise(res)) {
+                return res;
+            }
+            return new Promise((resolve, reject) => {
+                res.then((res) => {
+                    if (res[0]) {
+                        reject(res[0]);
+                    } else {
+                        resolve(res[1]);
+                    }
+                });
+            });
+        },
+    });
+
+} catch (error) {
+}
 
 const app = new Vue({
-  ...App
+    ...App
 })
 app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
 // #endif
---
+++ store/index.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+console.log(Vue);
+console.log(Vuex);
