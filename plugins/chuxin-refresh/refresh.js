06fe1be09c6186b7596120753f5843d32cfb5f0f
云层上的光 <15115359055@139.com>
2023-03-03 21:12:12 +0800

    feat: 上拉刷新和下拉刷新功能完善~

---
+++ plugins/chuxin-refresh/refresh.js
@@ -0,0 +1,62 @@
+class Refresh {
+    constructor(options) {
+        this.page = 0;
+        this.options = options;
+
+        this.params = null;
+        this.request = null;
+    }
+
+    /**
+     * @author 初心
+     * @desc 列表请求
+     */
+    getList(params = {}) {
+        this.params = params;
+        return async (request) => {
+            this.request = request || function () {
+            };
+            return await this.query();
+        }
+    }
+
+    /**
+     * @author 初心
+     * @desc 内部真正接口请求
+     */
+    async query() {
+        // 网络请求兜底
+        try {
+            // 每次请求 page++
+            this.page = this.page + 1;
+            // 列表请求
+            const res = await this.request({...this.params, page: this.page});
+            // 返回值
+            return {
+                res, page: this.page
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    /**
+     * @author 初心
+     * @desc 请求重置
+     */
+    reset() {
+        this.page = 0;
+    }
+
+    /**
+     * @author 初心
+     * @desc 下拉刷新
+     */
+    async refresh() {
+        this.reset();
+        await this.query();
+    }
+}
+
+
+export default Refresh;
